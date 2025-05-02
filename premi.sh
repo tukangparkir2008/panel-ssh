72aad9583c329bdb4f77ab9e3f7a65d9fed1fdd2
tukangparkir2008 <163428848+tukangparkir2008@users.noreply.github.com>
2025-05-03 06:14:49 +0700

    Update premi.sh

--- premi.sh
+++ premi.sh
@@ -94,10 +94,7 @@ clear
 if [ "${EUID}" -ne 0 ]; then
 		echo "You need to run this script as root"
 		exit 1
-fi
-if [ "$(systemd-detect-virt)" == "openvz" ]; then
-		echo "OpenVZ is not supported"
-		exit 1
+
 fi
 red='\e[1;31m'
 green='\e[0;32m'
